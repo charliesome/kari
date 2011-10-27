49e0e2bdc733b7a2340b2bb34b11a8e5fd46a6aa
charliesome <charlie@charliesomerville.com>
2011-10-27 20:09:23 +1100

    replaced kari_dict with st_table for the variable table

--- src/context.c
+++ src/context.c
@@ -7,20 +7,20 @@
 kari_context_t* kari_create_std_context()
 {    
     kari_context_t* ctx = (kari_context_t*)GC_MALLOC(sizeof(kari_context_t));
-    ctx->variables = new_kari_dict();
+    ctx->variables = st_init_strtable();
     ctx->parent = NULL;
     
     kari_load_stdlib(ctx);
     return ctx;
 }
 
-#define EXPOSE(fn) kari_dict_set(context->variables, #fn, kari_create_native_function(context, kari_stdlib_##fn, NULL))
+#define EXPOSE(fn) st_insert(context->variables, (st_data_t)#fn, (st_data_t)kari_create_native_function(context, kari_stdlib_##fn, NULL))
 void kari_load_stdlib(kari_context_t* context)
-{    
-    kari_dict_set(context->variables, "true", kari_true());
-    kari_dict_set(context->variables, "false", kari_false());
-    kari_dict_set(context->variables, "nil", kari_nil());
-    kari_dict_set(context->variables, "$inc", kari_create_array());
+{
+    st_insert(context->variables, (st_data_t)"true",    (st_data_t)kari_true());
+    st_insert(context->variables, (st_data_t)"false",   (st_data_t)kari_false());
+    st_insert(context->variables, (st_data_t)"nil",     (st_data_t)kari_nil());
+    st_insert(context->variables, (st_data_t)"$inc",    (st_data_t)kari_create_array());
     
     #undef K_FN
     #undef _KARI_STDLIB
@@ -46,10 +46,10 @@ kari_value_t* kari_call(kari_value_t* function, kari_value_t* argument, char** e
     switch(function->type) {
         case KARI_FUNCTION:
             ctx = (kari_context_t*)GC_MALLOC(sizeof(kari_context_t));
-            ctx->variables = new_kari_dict();
+            ctx->variables = st_init_strtable();
             ctx->parent = ((kari_function_t*)function)->parent;
             
-            kari_dict_set(ctx->variables, ((kari_function_t*)function)->argument, argument);
+            st_insert(ctx->variables, (st_data_t)((kari_function_t*)function)->argument, (st_data_t)argument);
             return kari_execute(ctx, ((kari_function_t*)function)->tokens, ((kari_function_t*)function)->token_count, err);
         case KARI_NATIVE_FUNCTION:
             *err = NULL;
